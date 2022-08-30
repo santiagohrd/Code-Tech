22841697ce0ed0c0b4e721a4e83ab42c3ffe4b20
LuFeBoAv <110979313+LuFeBoAv@users.noreply.github.com>
2022-08-29 22:17:29 -0500

    ejecucion

--- src/main/java/com/Admi/Tech/TechApplication.java
+++ src/main/java/com/Admi/Tech/TechApplication.java
@@ -4,7 +4,9 @@ import com.Admi.Tech.Modelo.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TechApplication {
 
@@ -12,10 +14,10 @@ public class TechApplication {
 		SpringApplication.run(TechApplication.class, args);
 	}
 
-	@GetMapping ("/Test")
+	@GetMapping ("/Empresa")
 	public String test(){
 		Empresa emp = new Empresa("Santiago SAS", "Calle 100", "2351397","8032542-4");
-		emp.setNombre("SOLAR LTDA");
+		//emp.setNombre("SOLAR LTDA");
 		return emp.getNombre();
 	}
 }
