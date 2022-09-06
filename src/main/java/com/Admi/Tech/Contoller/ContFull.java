b9b0fe6c17cb3faf1956c53aab1c89858645ae58
LuFeBoAv <110979313+LuFeBoAv@users.noreply.github.com>
2022-09-06 18:19:10 -0500

    Realizando metodos de empresa en el contfull

--- src/main/java/com/Admi/Tech/Contoller/ContFull.java
+++ src/main/java/com/Admi/Tech/Contoller/ContFull.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.persistence.Entity;
 import java.util.List;
@@ -21,12 +23,21 @@ public class ContFull {
         model.addAttribute("factorylist",empresaList);
         return "verEmpresas";
     }
-//Post
+
     @GetMapping("/AgregarEmpresa")
     public String newEmpresa(Model model){
         Empresa fac = new Empresa();
         model.addAttribute("fac",fac);
         return "agregarEmpresa";
     }
+    //Post
+    @PostMapping("/GuardarEmpresa")
+    public String guardarEmpresa(Empresa fac, RedirectAttributes redirectAttributes){
+        if(servFactory.saOrUpFactory(fac)==true){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/AgregarEmpresa";
+    }
+
 
 }
