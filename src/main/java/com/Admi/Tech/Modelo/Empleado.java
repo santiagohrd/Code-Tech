11d61b1133510ec04fbe93de3a221c964a3ee59c
Santiago Herrera <96275927+santiagohrd@users.noreply.github.com>
2022-09-08 21:06:09 -0500

    Update Empleado.java
    Se eliminan atributos innecesarios del modelo empleado.

--- src/main/java/com/Admi/Tech/Modelo/Empleado.java
+++ src/main/java/com/Admi/Tech/Modelo/Empleado.java
@@ -1,8 +1,63 @@
 package com.Admi.Tech.Modelo;
 
-
-
-
 public class Empleado {
+    private int id;
+    private String full_name;
+    private String email;
+    private String rol;
+    private String empresa;
+
+    public Empleados() {
+    }
+
+    public Empleados(int id, String full_name, String email, String rol, String empresa) {
+      this.id = id;
+      this.full_name = full_name;
+      this.email = email;
+      this.rol = rol;
+      this.empresa = empresa;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getempresa() {
+        return empresa;
+    }
+
+    public void setempresa(String empresa) {
+        this.empresa = empresa;
+    }
+}
 
 }
