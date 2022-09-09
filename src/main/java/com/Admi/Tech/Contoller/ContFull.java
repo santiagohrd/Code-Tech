233c45392f5de88caeaa3f62777b7bfcf5fe007e
LuFeBoAv <110979313+LuFeBoAv@users.noreply.github.com>
2022-09-08 21:28:57 -0500

    controller eliminar y pom

--- src/main/java/com/Admi/Tech/Contoller/ContFull.java
+++ src/main/java/com/Admi/Tech/Contoller/ContFull.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -36,8 +37,10 @@ public class ContFull {
     @PostMapping("/GuardarEmpresa")
     public String guardarEmpresa(Empresa fac, RedirectAttributes redirectAttributes){
         if(servFactory.saOrUpFactory(fac)==true){
+            redirectAttributes.addAttribute("mensaje","Guardo correctamente");
             return "redirect:/VerEmpresas";
         }
+        redirectAttributes.addAttribute("mensaje","Hubo un error");
         return "redirect:/AgregarEmpresa";
     }
 //editar empresas
@@ -48,23 +51,25 @@ public class ContFull {
         return "editarEmpresa";
     }
 //actualizar empresa
-    @PostMapping("/ActualizarEmpresa")
-    public String upEmpresa(Empresa fac){
-        if(servFactory.saOrUpFactory(fac)){
-            return "redirect:/VerEmpresas";
-        }
-        return "redirect:/EditarEmpresa";
+@PostMapping("/ActualizarEmpresa")
+public String upEmpresa(@ModelAttribute("fac") Empresa fac, RedirectAttributes redirectAttributes){
+    redirectAttributes.addAttribute("mensaje","Actualizo correctamente");
+    if(servFactory.saOrUpFactory(fac)){
+        return "redirect:/VerEmpresas";
+    }
+    redirectAttributes.addAttribute("mensaje","Hubo un error en la actualizacion");
+    return "redirect:/EditarEmpresa";
     }
     //eliminar
-
-    @GetMapping("/EleminarEmpresa/{id}")
+    @GetMapping("/EliminarEmpresa/{id}")
     public String delEmpresa(@PathVariable Integer id){
         try {
             servFactory.deletFactory(id);
         }catch (Exception e){
-            return "redirect:/VerEmpresas";
+            return "redirect:/VerEmpresa";
         }
-        return "redirect:/VerEmpresas";
+        return "redirect:/VerEmpresa";
     }
+
 
 }
