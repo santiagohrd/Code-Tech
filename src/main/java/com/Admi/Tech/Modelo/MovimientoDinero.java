5c041dc3741efa816eb9c0b0f3fcbaaebc7d4014
Santiago Herrera <santiagohrd1@gmail.com>
2022-09-10 05:46:24 -0500

    Se sube nuevemente el modelo MovimientoDinero

--- src/main/java/com/Admi/Tech/Modelo/MovimientoDinero.java
+++ src/main/java/com/Admi/Tech/Modelo/MovimientoDinero.java
@@ -1,4 +1,48 @@
 package com.Admi.Tech.Modelo;
 
+
 public class MovimientoDinero {
+    private int id;
+    private String concepto;
+    private int cantidad;
+    private String user;
+
+    public MovimientoDinero(int id, String concepto, int cantidad, String user){
+        this.id = id;
+        this.concepto = concepto;
+        this.cantidad = cantidad;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
 }
